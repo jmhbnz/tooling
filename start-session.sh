5aeb7003a918a84eabb9f884ebbf4fb7bb4b3fda
James Blair <mail@jamesblair.net>
2020-05-02 16:03:41 +1200

    Started tmate session in background.

--- start-session.sh
+++ start-session.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-tmate -S /tmp/$USER.$USER.tmate new-session -s james -n james
-tmate tmate set-window-option -t james automatic-rename off 
+tmate -S /tmp/$USER.$USER.tmate new-session -s james -n james -d
+tmate -S /tmp/$USER.$USER.tmate set-window-option -t james automatic-rename off 
+tmate -S /tmp/$USER.$USER.tmate set-window-option -t james allow-rename off
