ed121109851032c3046742cc065e5c2c7157462b
James Blair <mail@jamesblair.net>
2020-05-31 21:25:46 +1200

    Added attach to session script.

--- kubemacs/start-session.sh
+++ kubemacs/start-session.sh
@@ -2,3 +2,4 @@
 tmate -S /tmp/$USER.$USER.tmate new-session -s $USER -n $USER -d
 tmate -S /tmp/$USER.$USER.tmate set-window-option -t $USER automatic-rename off 
 tmate -S /tmp/$USER.$USER.tmate set-window-option -t $USER allow-rename off
+tmate -S /tmp/$USER.$USER.tmate attach
