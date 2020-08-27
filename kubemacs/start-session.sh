866376c5bf8b18276ea61e3b6ae272a76287eba2
James Blair <mail@jamesblair.net>
2020-08-27 22:20:07 +1200

    Renamed socket file to align with humacs.

--- kubemacs/start-session.sh
+++ kubemacs/start-session.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-tmate -S /tmp/$USER.$USER.tmate new-session -s $USER -n $USER -d
-tmate -S /tmp/$USER.$USER.tmate set-window-option -t $USER automatic-rename off 
-tmate -S /tmp/$USER.$USER.tmate set-window-option -t $USER allow-rename off
-tmate -S /tmp/$USER.$USER.tmate attach
+tmate -S /tmp/$USER.tmate.tmate new-session -s $USER -n $USER -d
+tmate -S /tmp/$USER.tmate.tmate set-window-option -t $USER automatic-rename off 
+tmate -S /tmp/$USER.tmate.tmate set-window-option -t $USER allow-rename off
+tmate -S /tmp/$USER.tmate.tmate attach
