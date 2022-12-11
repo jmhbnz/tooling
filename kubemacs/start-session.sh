9dd227eeeab852fd14cf14bbc59a005ffb997bb3
James Blair <mail@jamesblair.net>
2022-12-11 16:09:12 +1300

    Remove defunct firefox and kubemacs directories.

--- kubemacs/start-session.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-tmate -S /tmp/$USER.tmate.tmate new-session -s $USER -n $USER -d
-tmate -S /tmp/$USER.tmate.tmate set-window-option -t $USER automatic-rename off 
-tmate -S /tmp/$USER.tmate.tmate set-window-option -t $USER allow-rename off
-tmate -S /tmp/$USER.tmate.tmate attach
